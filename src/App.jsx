d606e46a9572c14ccad60f9975ac50ae865dfeeb
sanyam-dev <sanyamjha19@gmail.com>
2021-11-16 01:03:32 +0530

    Forms

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import React, { useState } from "react";
 import Forms from "./components/Form/Forms";
 
 function App() {
-    const [user, setUser] = useState({});
-    return(
-        <Forms setUser = {setUser} userData = {user}/>
-    )
+	const [user, setUser] = useState({});
 
+	return (<Forms setUser={setUser} userData={user} />)
 }
 
 export default App;
--- src/components/Form/Forms.jsx
+++ src/components/Form/Forms.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './Forms.module.css'
+// import axios from 'axios'
 // import Modal from './Modal/Modal'
 //TODO: Implement Modals
 
 
 export default function Forms({ setUser, userData }) {
-    // let flagModal1 = false;
+
     const sendData = (e) => {
         e.preventDefault();
         const name = document.getElementById('name').value;
@@ -13,33 +14,42 @@ export default function Forms({ setUser, userData }) {
         const password = document.getElementById('password').value;
         if (name === "" || email === "" || password === "") {
             console.log("fill info please!");
-            // flagModal1 = true;
         }
         setUser({
             "name": name,
             "email": email,
             "password": password,
         })
-        console.log(userData)
-    }
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userData)
+        }
+        fetch('http://localhost:5000/api/user/register', requestOptions)
+            .then(response => {
+                if(response.status === 200)
+                window.alert("Successful Registration!")
+                console.log(userData)
+                console.log(response)
+                document.getElementById('name').value = '';
+                document.getElementById('email').value = '';
+                document.getElementById('password').value = '';
 
-    return (<>
-        <div className={styles.container}>
-            {/* {
-                useEffect(()=>{
-                    flagModal1 ?
-                        <div className={styles.modal}> <Modal /> </div> : <div></div>
-                }, [flagModal1])  
-            } */}
-            <form >
-                <div className={styles.centerDiv} >
-                    <input type='text' placeholder='name' id='name' />
-                    <input type='email' placeholder='email' id='email' />
-                    <input type='password' placeholder='password' id='password' />
-                    <button value="send" onClick={sendData}>Submit</button>
-                </div>
-            </form>
-        </div>
-    </>
+            }
+        )
+    }
+    return (
+        <>
+            <div className={styles.container}>
+                <form >
+                    <div className={styles.centerDiv} >
+                        <input type='text' placeholder='name' id='name' />
+                        <input type='email' placeholder='email' id='email' />
+                        <input type='password' placeholder='password' id='password' />
+                        <button value="send" onClick={sendData}>Submit</button>
+                    </div>
+                </form>
+            </div>
+        </>
     )
 }
